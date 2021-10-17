97dd374083d7567f1efd0ba1561bb76903552748
codeKAMAT <easykamat@gmail.com>
2021-10-17 22:47:21 +0530

    working with recipes

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -12,7 +12,15 @@ exports.homepage = async(req, res) => {
     try {
         const limitNumber = 5;
         const categories = await Category.find({}).limit(limitNumber);
-        res.render('index', { title: 'Kamat Recipe App - Home', categories});
+        const latest = await Recipe.find({}).sort({_id: -1}).limit(limitNumber);
+        const thai = await Recipe.find({ 'category': 'Thai' }).limit(limitNumber);
+        const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
+        const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
+
+
+        const food = {latest, thai, american, chinese };
+        
+        res.render('index', { title: 'Kamat Recipe App - Home', categories, food});
     } catch (error) {
         res.status(500).send({message: error.message || "Error Occured" });
     }
@@ -20,7 +28,7 @@ exports.homepage = async(req, res) => {
 
 /*
  * Get /categories
- * Homepage
+ * Categories
 */
 exports.exploreCategories = async(req, res) => {
     try {
@@ -34,4 +42,53 @@ exports.exploreCategories = async(req, res) => {
 
 
 
+/*
+ * Get /recipe/:id
+ * Recipe
+*/
+exports.exploreRecipe = async(req, res) => {
+    try {
+        let recipeId = req.params.id;
+        const recipe = await Recipe.findById(recipeId);
+        res.render('recipe', { title: 'Kamat Recipe App - Recipe', recipe});
+    } catch (error) {
+        res.status(500).send({message: error.message || "Error Occured" });
+    }
+}
+
+
+// async function insertDummyRecipeData() {
+//     try {
+//         await Recipe.insertMany([
+//                   { 
+//                     "name": "Recipe Name Goes Here",
+//                     "description": `Recipe Description Goes Here`,
+//                     "email": "hello@codekamat.me",
+//                     "ingredients": [
+//                       "1 level teaspoon baking powder",
+//                       "1 level teaspoon cayenne pepper",
+//                       "1 level teaspoon hot smoked paprika",
+//                     ],
+//                     "category": "American", 
+//                     "image": "southern-friend-chicken.jpg"
+//                   },
+//                   { 
+//                     "name": "Recipe Name Goes Here",
+//                     "description": `Recipe Description Goes Here`,
+//                     "email": "hello@codekamat.me",
+//                     "ingredients": [
+//                       "1 level teaspoon baking powder",
+//                       "1 level teaspoon cayenne pepper",
+//                       "1 level teaspoon hot smoked paprika",
+//                     ],
+//                     "category": "American", 
+//                     "image": "southern-friend-chicken.jpg"
+//                   },
+//                 ]);
+//     } catch(error){
+//         console.log('err', + error);
+//     }
+// }
+
+// insertDummyRecipeData();
 
--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -5,6 +5,27 @@ const recipeSchema = new mongoose.Schema({
         type: String,
         require: 'This field is required'
     },
+    description: {
+        type: String,
+        require: 'This field is required'
+    },
+    email: {
+        type: String,
+        require: 'This field is required'
+    },
+    ingredients: {
+        type: Array,
+        require: 'This field is required'
+    },
+    category: {
+        type: String,
+        enum: ['Thai', 'American', 'Chinese', 'Mexican', 'Indian'],
+        require: 'This field is required'
+    },
+    image: {
+        type: String,
+        require: 'This field is required'
+    },
 })
 
 module.exports = mongoose.model('Recipe', recipeSchema);
